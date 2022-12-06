78baa875b43007c26e14437834b805fd052c00d4
Andrew Fitch <andrew.fitch@c3.ai>
2022-12-05 22:47:38 -0800

    extension working

--- src/scripts/content.js
+++ src/scripts/content.js
@@ -1,22 +1,10 @@
-
 // Try to get search result to highlight
 chrome.storage.local.get('curResult', function (result) {
     // if a search result is present in local storage, highlight it
     if (result != null && result.curResult) {
-        highlight(result.curResult)
+        window.find(result.curResult, false, false, true)
     } else {
         // send document text back to popup script
         chrome.runtime.sendMessage({docText: document.body.innerText});
     }
 });
-
-function highlight(text) {
-    console.log(text)
-    let inputText = document.body;
-    let innerHTML = inputText.innerHTML;
-    let index = innerHTML.indexOf(text);
-    if (index >= 0) {
-        innerHTML = innerHTML.substring(0, index) + "<span class='highlight'>" + innerHTML.substring(index, index + text.length) + "</span>" + innerHTML.substring(index + text.length);
-        inputText.innerHTML = innerHTML;
-    }
-}
